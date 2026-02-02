bd0f6d2216efd89652a198973b98f0274c2f2e00
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2026-02-02 05:25:55 -0500

    early morning changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1>Welcome to RM Web Solutions</h1>;
+  return (
+    <main>
+      <section className="grid grid-cols-2 gap-2 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div>
+          <h3 className="text-orange-500 text-2xl font-bold p-2">
+            Professional Web Solutions
+          </h3>
+          <h3 className="text-gray-800 text-2xl font-bold p-2">
+            To Boost your Business
+          </h3>
+        </div>
+      </section>
+    </main>
+  );
 }
