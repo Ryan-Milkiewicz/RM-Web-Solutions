cea333bc2303f5d1e994f3c156b3cb0b63702ddc
Ryan <ryanmilkiewicz@gmail.com>
2026-02-03 11:06:45 -0500

    more header work

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import ButtonWithSwoosh from "./ButtonSwoosh";
+
 export default function Header() {
   return (
-    <section className="grid grid-cols-2 gap-6 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="space-y-2">
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-4 items-center">
+      <div className="space-y-2 mb-16">
         <h3 className="text-orange-500 text-2xl font-bold p-2">
           Professional Web Solutions
         </h3>
@@ -13,12 +16,17 @@ export default function Header() {
           that help your business stand out, streamline operations, and turn
           ideas into real results.
         </p>
-        <button className="mt-6 bg-orange-500 rounded-lg text-white font-bold px-6 py-3">
-          Contact Us
-        </button>
+        <ButtonWithSwoosh />
       </div>
-      <div>
-        <h1>Image will go here</h1>
+      <div className="flex items-center justify-center">
+        <Image
+          src="/banner-image.jpg"
+          alt="Banner Image"
+          width={450}
+          height={450}
+          className="cursor-pointer object-contain"
+          priority
+        />
       </div>
     </section>
   );
