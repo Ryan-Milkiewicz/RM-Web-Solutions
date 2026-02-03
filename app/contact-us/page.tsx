effccf8ea47d9836520b5d5e3d0693da8ad43060
Ryan <ryanmilkiewicz@gmail.com>
2026-02-03 15:56:59 -0500

    added contact form and email send

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+import { sendEmail, State } from "../lib/mail";
+import { useActionState } from "react";
+
 export default function Page() {
+  const initialState: State = { message: null, errors: {} };
+  const [state, formAction, isLoading] = useActionState(
+    sendEmail,
+    initialState,
+  );
+
   return (
     <section
       id="contact"
-      className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8"
+      className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-32"
     >
       <h3 className="text-orange-500 text-2xl font-bold mb-2 text-center">
         Contact Us
@@ -12,19 +22,32 @@ export default function Page() {
         back to you.
       </p>
 
-      <form className="bg-white rounded-xl shadow-md p-6 space-y-6">
+      <form
+        action={formAction}
+        className="bg-white rounded-xl shadow-lg p-8 space-y-8"
+      >
         <div>
           <label className="block text-gray-700 font-medium mb-1">
             Full Name
           </label>
           <input
             type="text"
+            id="fullName"
             name="fullName"
             defaultValue=""
             placeholder="Your full name"
             className="w-full rounded-lg border border-gray-300 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-orange-500"
-            required
           />
+
+          {state?.errors?.fullName &&
+            state.errors.fullName.map((error: string) => (
+              <p
+                className="mt-1 text-sm text-orange-500 font-medium"
+                key={error}
+              >
+                {error}
+              </p>
+            ))}
         </div>
 
         <div>
@@ -37,8 +60,17 @@ export default function Page() {
             defaultValue=""
             placeholder="you@example.com"
             className="w-full rounded-lg border border-gray-300 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-orange-500"
-            required
           />
+
+          {state?.errors?.email &&
+            state.errors.email.map((error: string) => (
+              <p
+                className="mt-1 text-sm text-orange-500 font-medium"
+                key={error}
+              >
+                {error}
+              </p>
+            ))}
         </div>
 
         <div>
@@ -51,8 +83,17 @@ export default function Page() {
             defaultValue=""
             placeholder="(555) 123-4567"
             className="w-full rounded-lg border border-gray-300 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-orange-500"
-            required
           />
+
+          {state?.errors?.phoneNumber &&
+            state.errors.phoneNumber.map((error: string) => (
+              <p
+                className="mt-1 text-sm text-orange-500 font-medium"
+                key={error}
+              >
+                {error}
+              </p>
+            ))}
         </div>
 
         <div>
@@ -65,16 +106,31 @@ export default function Page() {
             defaultValue=""
             placeholder="Tell us about your project..."
             className="w-full rounded-lg border border-gray-300 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-orange-500 resize-none"
-            required
           />
         </div>
 
         <button
           type="submit"
           className="w-full bg-orange-500 text-white font-bold py-3 rounded-lg hover:bg-orange-600 transition"
+          disabled={isLoading}
         >
           Send Message
         </button>
+
+        {state.status === "success" && (
+          <div className="mt-3 text-sm text-orange-500 font-medium">
+            {state.message}
+          </div>
+        )}
+
+        {state.message && state.status === "error" && (
+          <div
+            className="mt-3 text-sm text-orange-500 font-medium"
+            role="alert"
+          >
+            {state.message}
+          </div>
+        )}
       </form>
     </section>
   );
---
+++ app/lib/mail.ts
@@ -0,0 +1,69 @@
+"use server";
+import { flattenError, z } from "zod";
+import { Resend } from "resend";
+import { EmailTemplate } from "../components/email-template/EmailTemplate";
+
+const resend = new Resend(process.env.RESEND_API_KEY!);
+
+const contactFormSchema = z.object({
+  fullName: z.string().min(2, "Full Name is required"),
+  email: z.email("Invalid email address"),
+  phoneNumber: z.string().min(1, "Phone Number is required"),
+  message: z.string().optional(),
+});
+
+export type State = {
+  errors?: {
+    fullName?: string[];
+    email?: string[];
+    phoneNumber?: string[];
+    message?: string[];
+  };
+  message?: string | null;
+  status?: "success" | "error";
+};
+
+export async function sendEmail(
+  previousState: State,
+  formData: FormData,
+): Promise<State> {
+  // Validate form using Zod
+  const validatedFields = contactFormSchema.safeParse({
+    fullName: formData.get("fullName"),
+    email: formData.get("email"),
+    phoneNumber: formData.get("phoneNumber"),
+    message: formData.get("message"),
+  });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: flattenError(validatedFields.error).fieldErrors,
+      // message: "Missing Fields.",
+      status: "error",
+    };
+  }
+
+  // Prepare data for email send out
+  const { fullName, email, phoneNumber, message } = validatedFields.data;
+
+  // Send email via Resend
+  try {
+    const data = await resend.emails.send({
+      from: process.env.RESEND_TO!,
+      to: [process.env.RESEND_INBOX!],
+      replyTo: [email],
+      subject: "New Project Inquiry",
+      react: EmailTemplate({
+        fullName,
+        email,
+        phoneNumber,
+        message,
+      }),
+    });
+    return { message: "Email Send Successfully.", status: "success" };
+  } catch (error) {
+    console.log(error);
+    return { message: "Email Error. Email Failed to send.", status: "error" };
+  }
+}
