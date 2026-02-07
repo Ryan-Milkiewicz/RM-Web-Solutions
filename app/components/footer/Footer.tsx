5402e3eae4469af817f772f9f08b8b49647613b8
RyanMilkiewicz <ryanmilkiewicz@gmail.com>
2026-02-07 09:12:16 -0500

    added border to top of footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-blue-950">
+    <footer className="bg-blue-950 border-t-orange-500 border-t-3">
       <div className="py-4 px-4 flex flex-col sm:flex-row items-center justify-center sm:space-x-4 space-y-2 sm:space-y-0 text-center">
         <span className="text-orange-500 text-md font-semibold">
           &copy; {new Date().getFullYear()} RM Web Solutions. All rights
