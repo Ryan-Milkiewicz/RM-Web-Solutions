37856e59080ec6f705d029efa62d65750f5e37ed
Ryan <ryanmilkiewicz@gmail.com>
2026-02-20 11:12:57 -0500

    adjust navbar sizing and logo

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -7,15 +7,15 @@ export default function Navbar() {
   return (
     <nav className="bg-blue-950 shadow-md border-b-[3px] border-b-orange-500">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-18 items-center">
+        <div className="flex justify-between h-24 items-center">
           {/* Logo */}
           <div className="shrink-0">
             <Link href="/">
               <Image
                 src="/rm-web-solutions-logo.png"
                 alt="RM Web Solutions Logo"
-                width={230}
-                height={230}
+                width={250}
+                height={250}
                 className="cursor-pointer drop-shadow-md transition-transform duration-300 hover:scale-105"
                 priority
               />
